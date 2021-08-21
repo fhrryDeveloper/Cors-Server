a80588a045a33a5e801efa3833c0c85d5b6c5a6f
fhrryDeveloper <fhrrydeveloper@gmail.com>
2021-08-21 14:45:41 +0300

    update: as useful

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Lang');
   next();
 });
 
-app.get('/jokes/random', (req, res) => {
+app.get('/getprices', (req, res) => {
   request(
-    { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    { url: 'https://api.integrator.io/v1/apis/601383a129da7d0877bc4eb2/request' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
